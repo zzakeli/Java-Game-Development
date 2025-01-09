7329fa8944bcdc062f091fb094232445fff90c3b
Jzy <jestalycastillo15@gmail.com>
2025-01-09 12:26:02 +0800

    added player ticker

--- Entity/Player.java
+++ Entity/Player.java
@@ -11,8 +11,9 @@ import Main.KeyHandler;
 
 public class Player extends Entity {
 
-    KeyHandler keyH;
-    GamePanel gamePanel;
+    private KeyHandler keyH;
+    private GamePanel gamePanel;
+    private int ticks = 10;
 
     public Player(KeyHandler keyH, GamePanel gamePanel) {
         this.keyH = keyH;
@@ -43,6 +44,13 @@ public class Player extends Entity {
     }
 
     public void update() {
+        movePlayer();
+        if (keyH.up || keyH.down || keyH.left || keyH.right) {
+            tickPlayer();
+        }
+    }
+
+    private void movePlayer() {
         double diagSpeed = speed / 4;
         if (keyH.up && keyH.right) {
             x += diagSpeed;
@@ -73,21 +81,53 @@ public class Player extends Entity {
         }
     }
 
-    public void draw(Graphics2D g2) {
-        BufferedImage playerImage = down1;
+    private void tickPlayer() {
+        tickCount++;
+        if (tickCount > ticks) {
+            if (spriteNum == 1) {
+                spriteNum = 2;
+            } else if (spriteNum == 2) {
+                spriteNum = 1;
+            }
+            tickCount = 0;
+        }
+    }
 
+    public void draw(Graphics2D g2) {
+
+        BufferedImage playerImage = down1;
         switch (direction) {
             case "up":
-                playerImage = up1;
+                if (spriteNum == 1) {
+                    playerImage = up1;
+                }
+                if (spriteNum == 2) {
+                    playerImage = up2;
+                }
                 break;
             case "down":
-                playerImage = down1;
+                if (spriteNum == 1) {
+                    playerImage = down1;
+                }
+                if (spriteNum == 2) {
+                    playerImage = down2;
+                }
                 break;
             case "left":
-                playerImage = left1;
+                if (spriteNum == 1) {
+                    playerImage = left1;
+                }
+                if (spriteNum == 2) {
+                    playerImage = left2;
+                }
                 break;
             case "right":
-                playerImage = right1;
+                if (spriteNum == 1) {
+                    playerImage = right1;
+                }
+                if (spriteNum == 2) {
+                    playerImage = right2;
+                }
                 break;
             default:
                 break;
@@ -96,4 +136,5 @@ public class Player extends Entity {
         g2.drawImage(playerImage, x, y, gamePanel.tileSize, gamePanel.tileSize, null);
 
     }
+
 }
